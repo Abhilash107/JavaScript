70beecae8271c79d94aec00864ebc1107dccdda3
Abhilash107 <abhilash00003108884@gmail.com>
2024-06-26 19:38:19 +0530

    dsa

---
+++ Dsa/rearrangeArray.js
@@ -0,0 +1,22 @@
+let arr = [3,1,-2,-5,2,-4]
+let newArr = arrangingArray(arr)
+console.log(newArr);
+
+
+function arrangingArray(arr){
+    let newArr = []
+    let pI =0, nI = 1
+    for(let i = 0;i<arr.length;i++){
+        if(arr[i]<0){
+            newArr[nI] = arr[i]
+            nI += 2
+        }
+        else{
+            newArr[pI] = arr[i]
+            pI += 2
+        }
+        
+        
+    }
+    return newArr
+}
