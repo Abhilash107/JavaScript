b257e7ccd7b418a8140475a6140551f4b91ab150
Abhilash107 <abhilash00003108884@gmail.com>
2024-07-01 11:05:32 +0530

    linkedList

--- LinkedList/creation.js
+++ LinkedList/creation.js
@@ -1,4 +1,5 @@
 class Node {
+    //* No need of initialization od data and next
     constructor(data){
         this.data = data
         this.next = null
@@ -7,6 +8,7 @@ class Node {
 
 class main{
     create(arr){
+        //* don't use Node variable name
         let head = new Node(arr[0])
         let mover = head
 
@@ -46,12 +48,36 @@ class main{
         }
         return count
     }
+
+    deleteHead(head){
+        if(head === null) return null
+        head = head.next
+        return head
+    }
+
+    deleteTail(head){
+        if(head === null || head.next === null){
+            return null
+        } 
+
+        let temp = head
+        while(temp.next.next !== null){
+            temp = temp.next
+        }
+        temp.next = null
+        return head
+    }
+
+    deleteAtAnyPosition(head){
+        
+    }
     
 }
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8]
+//* create an object of main() class
 const obj = new main()
-const head = obj.create(arr)// getting the head
+let head = obj.create(arr)// getting the head
 
 //obj.traverse(head)//traverse
 
@@ -59,4 +85,14 @@ const res = obj.search(head, 3)
 //console.log(res);
 
 const length = obj.length(head)
-console.log(length);
+//console.log(length);
+
+const newList1 = obj.deleteHead(head)
+//console.log(newList1);
+
+const newList2 = obj.deleteTail(head)
+//obj.traverse(newList2)
+
+
+
+
