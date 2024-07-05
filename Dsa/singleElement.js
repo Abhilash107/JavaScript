445a591638206f39c86fee156c03adae4e141580
Abhilash107 <abhilash00003108884@gmail.com>
2024-07-05 23:01:02 +0530

    gg

---
+++ Dsa/singleElement.js
@@ -0,0 +1,19 @@
+let arr = [1, 2, 2, 3, 3]
+
+const res =findSingleNum(arr)
+console.log(res);
+
+function findSingleNum(arr){
+    let i = 0
+    let j = arr.length-1
+    let mid
+
+    while(i<= j){
+        mid = (i+j)/2
+
+        
+        
+    }
+    return -1
+
+}
