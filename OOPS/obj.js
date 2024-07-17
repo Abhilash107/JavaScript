a70136e9e23bd9bfca22019ae410000265e07f96
Abhilash107 <abhilash00003108884@gmail.com>
2024-07-17 12:13:52 +0530

    prototype

--- OOPS/obj.js
+++ OOPS/obj.js
@@ -29,6 +29,18 @@ const newObj = Object.assign(player, coach)
 //? console.log(newObj);
 
 
+
+
+//*Object.create() => creates an object (empty)
+
+const newObj2 = Object.create(newObj)
+//console.log(newObj2); // empty Object
+newObj2.name = 'gg'
+newObj2.age = 28
+newObj2.club = 'JUV'
+console.log(newObj2);
+
+
 //*Object.freeze() => Freezes an object. A frozen object can no longer be changed
 
 Object.freeze(coach)
@@ -41,24 +53,48 @@ coach.age = 56
 const car = {
     make: 'Toyota',
     model: 'Corolla'
-  };
+};
   
-  // Seal the car object
-  Object.seal(car);
+// Seal the car object
+Object.seal(car);
   
-  // Attempt to add a new property
-  car.year = 2020; // This will fail
+// Attempt to add a new property
+car.year = 2020; // This will fail
   
-  // Attempt to delete an existing property
-  delete car.model; // This will also fail
+// Attempt to delete an existing property
+delete car.model; // This will also fail
   
-  // Modify an existing property
-  car.make = 'Honda'; // This will work if the property is writable
+// Modify an existing property
+car.make = 'Honda'; // This will work if the property is writable
   
-  console.log(car); // Output: { make: 'Honda', model: 'Corolla' }
+//console.log(car); // Output: { make: 'Honda', model: 'Corolla' }
   
 
 
+//! prototype....
+const obj2 = {
+    name: 'cool',
+    age: 25,
+}
+
+obj2.club = 'RMA'
+
+//console.log(obj2);
+//console.log(obj2.hasOwnProperty('club'))
+
+
+function CreateUser(userName, email, password){
+    this.setUserName(userName);
+    this.email = email;
+    this.password = password;
+}
+
+CreateUser.prototype.setUserName = function(userName) {
+    this.userName = userName;
+}
+
+const user = new CreateUser('Abhilash', 'wbqwbh@gmai.com', '2d3rt');
+console.log(user);
 
 
 
