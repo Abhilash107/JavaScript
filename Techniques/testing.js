48c6100c48f70bbb869d93805591f4946228dcb0
Abhilash107 <abhilash00003108884@gmail.com>
2024-07-23 20:58:54 +0530

    mocha

--- Techniques/testing.js
+++ Techniques/testing.js
@@ -1,5 +1,15 @@
-const sum = (a, b) => a + b;
+// const sum = (a, b) => a + b;
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
+
+const sum = require('../sum');
+const { expect } = require('chai');
+
+describe('Sum function', () => {
+  it('should add 1 and 2 to equal 3', () => {
+    expect(sum(1, 2)).to.equal(3);
+  });
 });
+
