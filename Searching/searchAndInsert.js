dae4b56025a2ac2c04574e2b4338654cdc7ee6e0
Abhilash107 <abhilash00003108884@gmail.com>
2024-09-12 18:43:44 +0530

    dsa

---
+++ Searching/searchAndInsert.js
@@ -0,0 +1,28 @@
+//? Same aa lower bound 
+
+const arr = [1, 2, 3, 3, 5, 5, 6, 7]
+const res = findAndInsert(arr, 4)
+
+console.log(res);
+
+
+function findAndInsert(a, x){
+    let l =0
+    let h = a.length -1
+    let ans = a.length
+
+    while(l <= h){
+
+        let mid = Math.floor(l + (h -l)/2);
+
+        if(a[mid] >= x) {
+            ans = mid
+            h = mid -1
+        }
+        else{
+            l = mid +1
+        }
+    }
+    return ans;
+
+}
