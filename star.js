1ba30691068013fbdf26593d52a9b477309f80ae
Abhilash107 <abhilash00003108884@gmail.com>
2024-09-27 13:54:24 +0530

    fun with Js

---
+++ star.js
@@ -0,0 +1,19 @@
+
+for(let i =0;i<5;i++){
+    let str = ""
+    for(let j = 1; j<=i; j++){
+        str +="*"
+    }
+    console.log(str, "\n");
+}
+
+for(let i =0;i<5;i++){
+    let str = ""
+    for(let j = i; j<5; j++){
+        str +="*"
+    }
+    console.log(str);
+}
+
+
+
